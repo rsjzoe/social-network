5108a8f5d1575f8816e46830d2e1182f43a632f2
rsjzoe <rsjzoe@gmail.com>
2024-11-21 10:40:15 +0300

    change text button

--- frontend/src/features/login/components/CreerCompte.jsx
+++ frontend/src/features/login/components/CreerCompte.jsx
@@ -95,7 +95,7 @@ export function CreerCompte({ onclick }) {
           >
             Log in
           </span>
-          <button>Sing on</button>
+          <button>Sing up</button>
         </form>
       </div>
     </>
